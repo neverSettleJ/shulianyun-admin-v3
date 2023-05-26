678de92e22a403e674e2c122a2053b04098f0add
jwk <1450234096@qq.com>
2023-05-26 15:53:51 +0800

    提交

--- src/router/sysmain.js
+++ src/router/sysmain.js
@@ -3,34 +3,34 @@
 const EmptyRouter = () => import('@/common/empty-router')
 
 const MerchantAccount = () =>
-  import('../views/merchant/account/accountindex.vue')
-const FinancePayment = () => import('@/views/finance/payment')
-const FinanceOrder = () => import('@/views/finance/order')
+	import('../views/merchant/account/accountindex.vue')
+// const FinancePayment = () => import('@/views/finance/payment')
+// const FinanceOrder = () => import('@/views/finance/order')
 
 export default {
-  path: '',
-  component: EmptyRouter,
-  children: [
-    {
-      path: '/merchant/account/accountindex',
-      component: MerchantAccount,
-      meta: {
-        requireAuth: true
-      }
-    },
-    {
-      path: 'finance/payment/index',
-      component: FinancePayment,
-      meta: {
-        requireAuth: true
-      }
-    },
-    {
-      path: 'finance/order/index',
-      component: FinanceOrder,
-      meta: {
-        requireAuth: true
-      }
-    }
-  ]
+	path: '',
+	component: EmptyRouter,
+	children: [
+		{
+			path: '/merchant/account/accountindex',
+			component: MerchantAccount,
+			meta: {
+				requireAuth: true
+			}
+		},
+		// {
+		//   path: 'finance/payment/index',
+		//   component: FinancePayment,
+		//   meta: {
+		//     requireAuth: true
+		//   }
+		// },
+		// {
+		//   path: 'finance/order/index',
+		//   component: FinanceOrder,
+		//   meta: {
+		//     requireAuth: true
+		//   }
+		// }
+	]
 }
